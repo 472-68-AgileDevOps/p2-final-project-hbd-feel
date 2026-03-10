5a048a1144f5a3714fe20968c752dbf91c893aba
Tonwantpillow <kerdsirija@gmail.com>
2026-03-10 09:38:29 +0700

    add features: edit application & delete application

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -33,6 +33,18 @@ class Application extends Model
         'status' => ApprovalStatus::class,
     ];
 
+    protected $appends = ['is_within_event_date'];
+
+    public function getIsWithinEventDateAttribute()
+    {
+        if (!$this->event || !$this->event->start_date || !$this->event->end_date) {
+            return false;
+        }
+
+        $now = now();
+        return $now->gte($this->event->start_date) && $now->lte($this->event->end_date);
+    }
+
     public function scopeWhereEventStatus($query, string $status, ?User $user = null)
     {
         if ($user && $user->role === UserRole::NISIT) {
--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,8 @@ Route::post('/login', [AuthenticateController::class, 'login'])->name('user.logi
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/applications', [ApplicationController::class, 'store']);
+    Route::put('/application/{id}', [ApplicationController::class, 'update']);
+    Route::delete('/application/{id}', [ApplicationController::class, 'destroy']);
     Route::get('/applications', [ApplicationController::class, 'getAllApplications']);
     Route::get('/applications/count', [ApplicationController::class, 'getApplicationCountByStatus']);
     Route::get('/applications/count/inprogress', [ApplicationController::class, 'getApplicationCountInprogress']);
